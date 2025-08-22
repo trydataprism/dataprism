f2a5f369e86faea6cfe9d8d9f90fd42ed4781873
chefberke <berkekanber@gmail.com>
2025-08-22 15:45:08 +0300

    refactor(dashboard): restructure DashboardLayout by adding Breadcrumb component and Sidebar elements for improved navigation

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,4 +1,16 @@
+"use client";
+
 import { Poppins } from "next/font/google";
+import { AppSidebar } from "@/components/sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -11,5 +23,33 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className={`${poppins.className}`}>{children}</div>;
+  return (
+    <div className={`${poppins.className}`}>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2">
+            <div className="flex items-center gap-2 px-4">
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">logo</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem className="hidden md:block">
+                    <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>All Websites</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+          {children}
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
 }
--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,46 +1,9 @@
-"use client";
-
-import { AppSidebar } from "@/components/sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { SearchBar } from "@/components/search-bar";
 
 export default function Page() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">logo</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>All Websites</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0 bg-muted/25 rounded-xl border border-muted/60 mb-5 mr-5">
-          {/* <SearchBar /> */}
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <div className="flex flex-1 flex-col gap-4 p-4 pt-0 bg-muted/25 rounded-xl border border-muted/60 mb-5 mr-5">
+      {/* <SearchBar /> */}
+    </div>
   );
 }
